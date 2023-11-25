fa405d583d2af97b31429bab5e51a262aaf404e9
jamie082 <jamiemorrissey02@gmail.com>
2023-11-25 00:25:36 -0800

    added files

--- java_ai.java
+++ java_ai.java
@@ -10,12 +10,12 @@ class loop{
         String String_2 = "Should there be higher taxes for Republicans who make over $450,000 yearly?";
         String String_3 = "Do you think there should be free health insurance for American Citizens every month?";
         String String_4 = "Which political party are you affilitated with?";
-
+        Scanner myObj = new Scanner(System.in);
         do {
             try {
                 // Creates a FileWriter
                 FileWriter output_1 = new FileWriter("data_1.txt", true);
-                Scanner myObj = new Scanner(System.in);
+
                 System.out.println("Should there be lower taxes for democrats?"); // write to data file
                 String input_1 = myObj.nextLine(); // get input
                 output_1.write(String_3 + "|" + input_1 + "\n"); // concentate string and output it to data_1 file
@@ -25,16 +25,23 @@ class loop{
             catch (Exception e) {
                 e.getStackTrace();
             }
- /* 
-            System.out.println("Should there be higher taxes for republcians who make over $450,000 yearly?"); // write to data file
-            String input_2 = myObj.nextLine();
 
-            System.out.println("Do you think there should be free health insurance for US Citizens?"); // write to data file
-            String input_3 = myObj.nextLine();
+            try {
+                System.out.println("Should there be higher taxes for republcians who make over $450,000 yearly?"); // write to data file
+                String input_2 = myObj.nextLine();
+                output_2.write(String_2 + "|" + input_2 + "\n");
+                output_2.close();
+            }
+
+            catch(Exception e) {
+                e.getStackTrace();
+            }
+    
         
             System.out.println("Which political party are you affiliated with?"); // write to data file
-            String input_4 = myObj.nextLine();*/
+            String input_4 = myObj.nextLine();
             i++;
+        
         } while (i < 1);
         // load data file at the end
     }
